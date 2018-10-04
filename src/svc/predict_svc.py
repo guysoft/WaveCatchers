503b05434db8eb33b1afbebbe64b895f74e0479e
Guy Sheffer <guysoft@gmail.com>
2018-10-05 01:29:47 +0300

    Make predict work

--- src/svc/predict_svc.py
+++ src/svc/predict_svc.py
@@ -1,11 +1,11 @@
-from train_svc import load_sound_file_for_network, load_dataset, FREQUENCY_UNIT_COUNT
+from train_svc import load_sound_file_for_network, load_dataset, FREQUENCY_UNIT_COUNT, labels, MODEL_PATH
 import os
 import numpy as np
 import pickle
 from get_time_series import get_file
 
 if __name__ == "__main__":
-    with open("/tmp/svc.pkl", 'rb') as f:
+    with open(MODEL_PATH, 'rb') as f:
         clf = pickle.load(f)
 
     #test_file = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "dataset", "dataset4", "all_up", "chunk86-97.56637.wav"))
@@ -29,15 +29,14 @@ if __name__ == "__main__":
     test = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "dataset", "dataset4", "all_up", "chunk86-97.56637.wav"))
 
     x_data = np.zeros((1, FREQUENCY_UNIT_COUNT))
-    # import code; code.interact(local=dict(globals(), **locals()))
+    #
 
     data = get_file(test)
 
-    # x_data[0] = np.array(data[:min(FREQUENCY_UNIT_COUNT, len(data))])
-    print("TODO: load the actual vector to predict")
+    x_data[0] = np.array(data[1][:min(FREQUENCY_UNIT_COUNT, len(data[1]))])
 
     y_pred = clf.predict(x_data)
 
-    print(y_pred)
+    print(list(labels[x] for x in  y_pred))
 
     print("Done")
